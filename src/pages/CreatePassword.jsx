3dee87418c0d8fd2fd9bda24958a5a017c3ad251
Tanzeel <65041070+tanzeel-abdulWahid@users.noreply.github.com>
2023-01-23 03:52:18 +0500

    Create pass screen css fix

--- src/pages/CreatePassword.jsx
+++ src/pages/CreatePassword.jsx
@@ -9,25 +9,25 @@ import CustomButton from "../components/CustomButton"
 const CreatePassword = () => {
     return (
         <>
-            <div className="p-5 md:p-14 lg:p-0 flex ">
+            <div className="p-0 flex ">
                 {/* Left Side confirm email Comp */}
                 <div className="w-full lg:w-1/2">
                     <div className={`${styles.flexCenter} bg-darkBlue shadow-sm p-3`}>
                         <h1 className={`${styles.boldText} text-lightGreen`}>Your email has been verified!</h1>
                     </div>
 
-                    <div className={`${styles.flexCenter} flex-col`}>
-                        <div className="pt-28">
+                    <div className={`${styles.flexCenter} flex-col px-2`}>
+                        <div className="pt-16 md:pt-28 md:w-2/3">
 
-                            <h1 className={`${styles.boldText} text-4xl`}>Create Password</h1>
+                            <h1 className={`${styles.boldText} text-xl md:text-4xl`}>Create Password</h1>
 
                             <div className="text-gray-600 mb-6">
 
-                                <h1 className="pt-12 pb-3">Password</h1>
+                                <h1 className="pt-12 pb-3 text-xl">Password</h1>
                                 <InputField type={'password'} placeholder="Strong-password123#." />
 
                                 <h1 className="p-2">Password should have:</h1>
-                                <ul class="list-disc ml-4">
+                                <ul class="list-disc ml-4 md:text-sm text-lg">
                                     <li class="">Minimum characters of 8 length</li>
                                     <li class="">Atleast one uppercase characters is required</li>
                                     <li class="">Atleast one lowercase characters is required</li>
