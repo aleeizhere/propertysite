ef2447692f098d2c9de4c48f105b15e8c0c1b985
Tanzeel <65041070+tanzeel-abdulWahid@users.noreply.github.com>
2023-01-30 21:11:09 +0500

    propertyDetails page without  map

--- src/pages/PropertyPage/PropertyDetailsTab.jsx
+++ src/pages/PropertyPage/PropertyDetailsTab.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import { CalendarOutlined, HomeOutlined, LineChartOutlined, MoneyCollectOutlined, UserOutlined } from '@ant-design/icons'
-import { Divider, Slider } from 'antd'
+import { CalendarOutlined, CloseCircleOutlined, HomeOutlined, InfoOutlined, LineChartOutlined, MoneyCollectOutlined, UserOutlined } from '@ant-design/icons'
+import { Divider, Steps, Progress } from 'antd'
+import dumbbell from "../../assets/dumbbell.svg"
+import pool from "../../assets/pool.svg"
 import ImageSlider from "../../components/ImageSlider"
 import styles from '../../style'
 
@@ -10,10 +12,18 @@ const homeDetails = [
     { icon: <MoneyCollectOutlined />, heading: "Average daily rate of 500 Taka", desc: "Monthly rental income will vary based on daily rate and occupancy levels" },
     { icon: <CalendarOutlined />, heading: "80% average occupancy rates", desc: "Daily rates and occupancy levels will vary by season" },
     { icon: <LineChartOutlined />, heading: "14.26% projected annual gross yield", desc: "With an expected net yield of 7.05% and a price per square foot of 2,057 TAKA " },
-
-
+]
+const priceDetails = [
+    { heading: "Property Price", price: "736,154 TAKA" },
+    { heading: "Transaction Cost", price: "736,154" },
+    { heading: "Stake fees", price: "1.5%" },
 ]
 
+const valueAppreciation = [
+    { heading: "Value appreciation", percent: "30%" },
+    { heading: "Value appreciation", percent: "30%" },
+
+]
 const PropertyDetailsTab = () => {
     let propertyDesc = `Lorem ipsum dolor sit amet consectetur adipisicing elit. In officiis ratione id! Dolore voluptates neque similique ullam! Voluptatem beatae aliquam labore magni quibusdam expedita consectetur dolores corporis pariatur. Debitis, hic!
     Perspiciatis deserunt quidem quae molestiae cumque, sunt at molestias distinctio, fugiat quis qui. Voluptatum illo incidunt excepturi optio qui commodi eaque id eveniet vitae, labore, eligendi ducimus. Laudantium, quaerat repellendus!
@@ -42,19 +52,16 @@ const PropertyDetailsTab = () => {
 
 
     return (
-        <div>
+        <div className=''>
             <h1 className={`${styles.boldText} text-lightGreen text-2xl mb-6`}>Portfolio {'>'} <span className='font-light text-base'> {"Property Name"} </span> </h1>
             <ImageSlider />
 
             <div className="flex flex-col-reverse md:flex-row">
                 <div className="md:w-[65%] md:p-5 p-2">
 
-
-
-
                     {/* Left Div Content */}
-                    <div>
-                        <h1 className={`${styles.boldText} text-lightGreen font-bold md:text-3xl`}>Studio in Studio One - Dubai Marina</h1>
+                    <div className='pr-5'>
+                        <h1 className={`${styles.boldText} text-lightGreen font-bold md:text-3xl mt-8`}>Studio in Studio One - Dubai Marina</h1>
                         <h1 className='text-xs md:text-sm'>1 bath <span> <Divider type='vertical' className='border-black' /> </span> Studio <span> <Divider className='border-black' type='vertical' /> </span> 358 sq.ft <span> <Divider className='border-black' type='vertical' /> </span> Dubai Marina</h1>
 
                         <Divider className='border-black' />
@@ -87,41 +94,31 @@ const PropertyDetailsTab = () => {
                                 <p>{text}  {!readMore && '...'}</p>
                                 <p onClick={showMore} className="font-bold text-lightGreen cursor-pointer"> {readMore ? 'Show Less' : " Read More"}</p>
                             </div>
-
-
                             {/* Financial Details */}
                             <h1 className={`${styles.boldText} text-lightGreen font-bold md:text-3xl pt-12 pb-5`}>Financial Details</h1>
 
                             <div className='flex justify-between gap-x-5'>
                                 <div className='w-1/2 text-lg'>
                                     <h1 className='font-bold'>Propery Cost</h1>
-                                    <div className='flex justify-between '>
-                                        <h1 className='font-thin'>Property Price</h1>
-                                        <h1 className='font-semibold'>736,393 TAKA</h1>
-                                    </div>
-                                    <div className='flex justify-between'>
-                                        <h1 className='font-thin'>Transaction Cost</h1>
-                                        <h1 className='font-semibold'>736,393 TAKA</h1>
-                                    </div>
-                                    <div className='flex justify-between'>
-                                        <h1 className='font-thin'>Stake fees</h1>
-                                        <h1 className='font-semibold'>1.5%</h1>
-                                    </div>
+                                    {priceDetails.map((detail, i) => (
+                                        <div className='flex justify-between'>
+                                            <h1 className='font-thin'>{detail.heading}</h1>
+                                            <h1 className='font-semibold'>{detail.price}</h1>
+                                        </div>
+                                    ))}
                                     <Divider />
                                     <div className='flex justify-between'>
                                         <h1 className='font-thin'>Invesment Cost</h1>
                                         <h1 className='font-semibold'>855,856 TAKA</h1>
                                     </div>
 
-                                    <div className='flex justify-between mt-4'>
-                                        <div className='bg-white p-3 rounded-lg text-center'>
-                                            <h1 className='font-thin'>Value appreciation</h1>
-                                            <h1 className='font-semibold'>30%</h1>
-                                        </div>
-                                        <div className='bg-white p-3 rounded-lg text-center '>
-                                            <h1 className='font-thin'>Value appreciation</h1>
-                                            <h1 className='font-semibold'>30%</h1>
-                                        </div>
+                                    <div className='flex flex-wrap lg:flex-nowrap gap-y-3 justify-between mt-4'>
+                                        {valueAppreciation.map((value, i) => (
+                                            <div key={i} className='bg-white p-3 rounded-lg text-center'>
+                                                <h1 className='font-thin'>{value.heading}</h1>
+                                                <h1 className='font-semibold'>{value.percent}</h1>
+                                            </div>
+                                        ))}
                                     </div>
 
                                 </div>
@@ -129,38 +126,85 @@ const PropertyDetailsTab = () => {
 
                                 <div className='w-1/2 text-lg'>
                                     <h1 className='font-bold'>Propery Cost</h1>
-                                    <div className='flex justify-between '>
-                                        <h1 className='font-thin'>Property Price</h1>
-                                        <h1 className='font-semibold'>736,393 TAKA</h1>
-                                    </div>
-                                    <div className='flex justify-between'>
-                                        <h1 className='font-thin'>Transaction Cost</h1>
-                                        <h1 className='font-semibold'>736,393 TAKA</h1>
-                                    </div>
-                                    <div className='flex justify-between'>
-                                        <h1 className='font-thin'>Stake fees</h1>
-                                        <h1 className='font-semibold'>1.5%</h1>
-                                    </div>
+                                    {priceDetails.map((detail, i) => (
+                                        <div className='flex justify-between'>
+                                            <h1 className='font-thin'>{detail.heading}</h1>
+                                            <h1 className='font-semibold'>{detail.price}</h1>
+                                        </div>
+                                    ))}
                                     <Divider />
                                     <div className='flex justify-between'>
                                         <h1 className='font-thin'>Invesment Cost</h1>
                                         <h1 className='font-semibold'>855,856 TAKA</h1>
                                     </div>
 
-                                    <div className='flex justify-between mt-4'>
-                                        <div className='bg-white p-3 rounded-lg text-center'>
-                                            <h1 className='font-thin'>Value appreciation</h1>
-                                            <h1 className='font-semibold'>30%</h1>
-                                        </div>
-                                        <div className='bg-white p-3 rounded-lg text-center '>
-                                            <h1 className='font-thin'>Value appreciation</h1>
-                                            <h1 className='font-semibold'>30%</h1>
-                                        </div>
+                                    <div className='flex flex-wrap gap-y-3 lg:flex-nowrap justify-between mt-4'>
+                                        {valueAppreciation.map((value, i) => (
+                                            <div key={i} className='bg-white p-3 rounded-lg text-center'>
+                                                <h1 className='font-thin'>{value.heading}</h1>
+                                                <h1 className='font-semibold'>{value.percent}</h1>
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
+
                             </div>
 
+                            <h1 className='text-base pt-5 font-thin'>Projections based on a 5 year holding period. You can view the Investment Memorandum in the Documentation section for a full financial breakdown</h1>
 
+
+                            {/* Funnding TimeLine */}
+                            <h1 className={`${styles.boldText} text-lightGreen font-bold md:text-3xl pt-12 pb-5`}>Funding Timeline</h1>
+
+                            {/* Steps */}
+                            <Steps
+                                progressDot
+                                current={2}
+                                direction="vertical"
+                                size='default'
+                                className='font-bold'
+                                items={[
+                                    {
+                                        title: 'January 12th, 2023',
+                                        description: "Closing Date"
+                                    },
+                                    {
+                                        title: 'February 2nd, 2023',
+                                        description: 'SPV created and title deeds distributed',
+                                    },
+                                    {
+                                        title: 'February 9th, 2023',
+                                        description: 'DTCM application approved',
+                                    },
+                                    {
+                                        title: 'February 9th, 2023',
+                                        description: 'Handover to property manager',
+                                    },
+                                    {
+                                        title: 'April 27th, 2023',
+                                        description: 'Expected first rental payment',
+                                    },
+                                ]}
+                            />
+
+
+                            <h1 className={`${styles.boldText} text-lightGreen font-bold md:text-3xl pt-12 pb-5`}>Location</h1>
+                            <div>
+                                <h1 className='flex items-center gap-x-2 font-bold text-lg'> <span className='bg-white rounded-[100%] p-2'> <InfoOutlined /> </span> Dubai Marina, Dubai United Arab Emirates </h1>
+                                <h1 className='text-base pt-5 font-thin'>Dubai Marina is one of the premier neighbourhoods in Dubai. This impressive community boasts luxury skyscrapers, a range of dining and entertainment options and urban waterfront living to its residents. It is the most popular area for renting luxury apartments in Dubai.</h1>
+                            </div>
+
+                            <h1 className={`${styles.boldText} text-lightGreen font-bold md:text-3xl pt-12 pb-5`}>Amenities</h1>
+                            <div className='flex justify-between md:w-64'>
+                                <div className='flex gap-x-4 items-center'>
+                                    <img src={dumbbell} alt="" />
+                                    <h1>Gym</h1>
+                                </div>
+                                <div className='flex gap-x-4 items-center'>
+                                    <img src={pool} alt="" />
+                                    <h1>Pool</h1>
+                                </div>
+                            </div>
 
                         </div>
 
@@ -175,12 +219,28 @@ const PropertyDetailsTab = () => {
 
 
 
-                <div className="w-full md:w-[35%] md:h-64 p-2 md:p-5 md:sticky md:top-0">
-                    {/* Right Div Content */}
-                    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo consequatur laudantium eligendi veniam, tenetur libero dicta facere dolore rerum ipsam excepturi voluptatibus id fugit sed veritatis, nostrum sunt voluptas quia?</h1>
-                    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo consequatur laudantium eligendi veniam, tenetur libero dicta facere dolore rerum ipsam excepturi voluptatibus id fugit sed veritatis, nostrum sunt voluptas quia?</h1>
-                    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo consequatur laudantium eligendi veniam, tenetur libero dicta facere dolore rerum ipsam excepturi voluptatibus id fugit sed veritatis, nostrum sunt voluptas quia?</h1>
+                <div className="w-full md:w-[35%] md:h-fit p-2 md:p-5 md:sticky md:top-6 mt-8 bg-white rounded-lg">
+
+                        <h1 className={`${styles.boldText} md:text-xl text-center`}>Property Price</h1>
+                        <h1 className={`${styles.boldText} text-lightGreen font-bold md:text-3xl pb-5 text-center mt-2`}>736,295 TAKA</h1>
+                        <Progress showInfo={false} percent={100} strokeColor="#4673B8" />
+                        <h1 className={`${styles.boldText} text-sm pb-5 `}>100% funded</h1>
+                        <div className='flex justify-between'>
+                            <h1 className={`${styles.boldText} text-sm`}> 111 Investors </h1>
+                            <h1 className='text-red-500 flex items-center gap-x-2'> {<CloseCircleOutlined />} Closed on 12 Jan 2023</h1>
+                        </div>
+                        <div className='bg-creamWhite p-3 rounded-lg mt-4'>
+                            <h1 className='font-bold'>Propery Cost</h1>
+                            {priceDetails.map((detail, i) => (
+                                <div className='flex justify-between py-2'>
+                                    <h1 className='font-thin'>{detail.heading}</h1>
+                                    <h1 className='font-semibold'>{detail.price}</h1>
+                                </div>
+                            ))}
+                        </div>
+                        
                 </div>
+
             </div>
         </div>
     )
